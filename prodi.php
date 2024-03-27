7ad96feb0f5ff101727f43b299dbf28cdb765b29
AUDIA APRIYANI <audiaap9975@gmail.com>
2024-03-27 14:29:36 +0800

    Pembaruan halaman mahasiswa dan prodi (penambahan database)

--- prodi.php
+++ prodi.php
@@ -1,18 +1,50 @@
 <?php
 include 'template/header.php';
-include 'template/sidebar.php';
-?>
-  <!-- /.navbar -->
+include 'template/sidebar.php' ;
 
-  <!-- Main Sidebar Container -->
- 
+$servername = "localhost";
+$database = "poliban";
+$username = "root";
+$password = "";
+
+$sconn = mysqli_connect($servername, $username, $password, $database);
+
+$query = "SELECT * FROM prodi";
+$hasil = mysqli_query($sconn, $query);
+
+$data = [];
+while ($baris = mysqli_fetch_assoc($hasil))
+{
+  $data[] = $baris;
+}
+
+?>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
-      <div class="row">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Data Program Studi</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
           <div class="col-12">
             <div class="card">
               <div class="card-header">
@@ -35,62 +67,41 @@ include 'template/sidebar.php';
                 <table class="table table-hover text-nowrap">
                   <thead>
                     <tr>
-                      <th>ID</th>
-                      <th>User</th>
-                      <th>Date</th>
-                      <th>Status</th>
-                      <th>Reason</th>
+                      <th>No</th>
+                      <th>Nama prodi</th>
+                      <th>Aksi</th>
                     </tr>
                   </thead>
                   <tbody>
+                    <?php
+                    foreach ($data as $d) {
+                    ?>
                     <tr>
-                      <td>183</td>
-                      <td>John Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-success">Approved</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                      <td>1</td>
+                      <td><?php echo $d['Nama_prodi']?> </td>
+                      <td><a href= "" class="btn btn-warning"> Edit</a>
+                      <a href="" class ="btn btn-danger">Hapus</a>
                     </tr>
-                    <tr>
-                      <td>219</td>
-                      <td>Alexander Pierce</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-warning">Pending</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                      <td>657</td>
-                      <td>Bob Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-primary">Approved</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
-                    <tr>
-                      <td>175</td>
-                      <td>Mike Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-danger">Denied</span></td>
-                      <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-                    </tr>
+                    <?php
+                    }
+                    ?>
                   </tbody>
                 </table>
               </div>
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
         
-        
-       
         <!-- /.row (main row) -->
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <?php
-  include 'template/footer.php';
-  ?>
+ <?php
+ include 'template/foother.php';
+ ?>
